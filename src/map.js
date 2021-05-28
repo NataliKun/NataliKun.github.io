5189644b8f6388c074d5b85cd03b619b3929ab04
NataliKun <kunnata@mail.ru>
2021-05-28 13:01:49 +0300

    карта настроена

--- src/map.js
+++ src/map.js
@@ -1,26 +1,21 @@
 /* global ymaps */
 
+const geolocation = require('./geolocation')
+
 // Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-function init () {
-  // Создание карты.
-  // const myMap =
-
-  // eslint-disable-next-line no-new
-  new ymaps.Map('map', {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [55.76, 37.64],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 7
-  })
-}
 
 function map () {
-  ymaps.ready(init)
+  geolocation.locationCoords().then((coords) => {
+    function init () {
+      // eslint-disable-next-line no-new
+      new ymaps.Map('map', {
+        center: coords,
+        zoom: 7
+      })
+    }
+    ymaps.ready(init)
+  })
 }
 
 module.exports = {
